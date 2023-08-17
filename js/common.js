341d373ef3ab9d1f5b797733dcd380696552c917
Fardin7864 <108412879+Fardin7864@users.noreply.github.com>
2023-08-17 21:58:17 +0600

    add new function and organiged the code

--- js/common.js
+++ js/common.js
@@ -19,13 +19,91 @@ function getMalti(a,b){
     const h = getInputValueById(b);
     return d*h;
 }
-// Set inner text by id
+// Set inner text by id from input
 function setInnerTextById(inputId,outputId){
     const inputVal = getInputValueByIdWt(inputId);
     const outputVal = document.getElementById(outputId);
     outputVal.innerText = inputVal;
 }
 
+// set inner text by id
+function setInnerTextByIdFromText(textFildId,area){
+    const inputFild = document.getElementById(textFildId);
+    inputFild.innerText = area;
+}
+
+
+// Triangle Calculete
+document.getElementById('btn-triangle').addEventListener('click',() => { 
+
+    //  Update base and height outputs using setInnerTextById
+     setInnerTextById('triangle-input-b', 'triangle-output-b');
+     setInnerTextById('triangle-input-h', 'triangle-output-h');
+    // Calculation
+    const x = getMalti('triangle-input-b','triangle-input-h');
+    const result = (x * 0.5);
+    setInnerTextByIdFromText('triangle-result',result);
+    
+ })
+
+// Rectangle Calculete
+document.getElementById('btn-rectangle').addEventListener('click',() => { 
+
+    //  Update base and height outputs using setInnerTextById
+    setInnerTextById('rectangle-input-w', 'rectangle-output-w');
+    setInnerTextById('rectangle-input-l', 'rectangle-output-l');
+
+    const x = getMalti('rectangle-input-w','rectangle-input-l');
+    setInnerTextByIdFromText('rectangle-result',x);
+ })
+ 
+ // Parallelogram Calculete
+ document.getElementById('btn-parallelogram').addEventListener('click',() => { 
+        //  Update base and height outputs using setInnerTextById
+        setInnerTextById('parallelogram-input-b', 'parallelogram-output-b');
+        setInnerTextById('parallelogram-input-h', 'parallelogram-output-h');
+
+     const x = getMalti('parallelogram-input-b','parallelogram-input-h');
+     setInnerTextByIdFromText('parallelogram-result',x);
+  })
+ 
+  // Rhombus Calculete
+document.getElementById('btn-rhombus').addEventListener('click',() => { 
+
+    //  Update base and height outputs using setInnerTextById
+    setInnerTextById('rhombus-input-b', 'rhombus-output-b');
+    setInnerTextById('rhombus-input-h', 'rhombus-output-h');
+
+    const x = getMalti('rhombus-input-b','rhombus-input-h');
+    const result = (x * 0.5);
+    setInnerTextByIdFromText('rhombus-result',result);
+
+ })
+
+  // Pentagon Calculete
+document.getElementById('btn-pentagon').addEventListener('click',() => { 
+    //  Update base and height outputs using setInnerTextById
+    setInnerTextById('pentagon-input-b', 'pentagon-output-b');
+    setInnerTextById('pentagon-input-h', 'pentagon-output-h');
+
+
+    const x = getMalti('pentagon-input-b','pentagon-input-h');
+    const result = (x * 0.5);
+    setInnerTextByIdFromText('pentagon-result',result);
+ })
+ 
+ // Ellipse Calculete
+document.getElementById('btn-ellipse').addEventListener('click',() => { 
+    //  Update base and height outputs using setInnerTextById
+    setInnerTextById('ellipse-input-b', 'ellipse-output-b');
+    setInnerTextById('ellipse-input-h', 'ellipse-output-h');
+
+
+   const x = getMalti('ellipse-input-b','ellipse-input-h');
+   const result = (x * 3.1416);
+   setInnerTextByIdFromText('ellipse-result',result);
+
+})
 
 // Get all buttons with the class 'btnCmToM'
 const buttons = document.querySelectorAll('.btnCmToM');
@@ -49,79 +127,4 @@ buttons.forEach((button, index) => {
         span.innerText = areaM2.toFixed(5) + 'm²';
     });
 });
-
-
-// Triangle Calculete
-document.getElementById('btn-triangle').addEventListener('click',() => { 
-
-    //  Update base and height outputs using setInnerTextById
-     setInnerTextById('triangle-input-b', 'triangle-output-b');
-     setInnerTextById('triangle-input-h', 'triangle-output-h');
-    // Calculation
-    const x = getMalti('triangle-input-b','triangle-input-h');
-    const result = (x * 0.5);
-    document.getElementById('triangle-result').innerText = result;
-    
- })
-
-// Rectangle Calculete
-document.getElementById('btn-rectangle').addEventListener('click',() => { 
-
-    //  Update base and height outputs using setInnerTextById
-    setInnerTextById('rectangle-input-w', 'rectangle-output-w');
-    setInnerTextById('rectangle-input-l', 'rectangle-output-l');
-
-    const x = getMalti('rectangle-input-w','rectangle-input-l');
-    document.getElementById('rectangle-result').innerText = x;
- })
- 
- // Parallelogram Calculete
- document.getElementById('btn-parallelogram').addEventListener('click',() => { 
-        //  Update base and height outputs using setInnerTextById
-        setInnerTextById('parallelogram-input-b', 'parallelogram-output-b');
-        setInnerTextById('parallelogram-input-h', 'parallelogram-output-h');
-
-
-     const x = getMalti('parallelogram-input-b','parallelogram-input-h');
-     document.getElementById('parallelogram-result').innerText = x;
-  })
- 
-  // Rhombus Calculete
-document.getElementById('btn-rhombus').addEventListener('click',() => { 
-
-    //  Update base and height outputs using setInnerTextById
-    setInnerTextById('rhombus-input-b', 'rhombus-output-b');
-    setInnerTextById('rhombus-input-h', 'rhombus-output-h');
-
-    const x = getMalti('rhombus-input-b','rhombus-input-h');
-    const result = (x * 0.5);
-    document.getElementById('rhombus-result').innerText = result;
-
- })
-
-  // Pentagon Calculete
-document.getElementById('btn-pentagon').addEventListener('click',() => { 
-    //  Update base and height outputs using setInnerTextById
-    setInnerTextById('pentagon-input-b', 'pentagon-output-b');
-    setInnerTextById('pentagon-input-h', 'pentagon-output-h');
-
-
-    const x = getMalti('pentagon-input-b','pentagon-input-h');
-    const result = (x * 0.5);
-    document.getElementById('pentagon-result').innerText = result;
-
- })
- 
- // Ellipse Calculete
-document.getElementById('btn-ellipse').addEventListener('click',() => { 
-    //  Update base and height outputs using setInnerTextById
-    setInnerTextById('ellipse-input-b', 'ellipse-output-b');
-    setInnerTextById('ellipse-input-h', 'ellipse-output-h');
-
-
-   const x = getMalti('ellipse-input-b','ellipse-input-h');
-   const result = (x * 3.1416);
-   document.getElementById('ellipse-result').innerText = result;
-
-})
 
